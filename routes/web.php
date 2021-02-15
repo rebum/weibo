3476edf6ab03df27b75f9cdfb9fa3ec8c385c4a7
vagrant <vagrant@homestead>
2021-02-15 15:13:05 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,5 @@ Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confi
 Route::get('password/reset','PasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email','PasswordController@sendResetLinkEmail')->name('password.email');
 
-Route::get('password/reset{token}','PasswordController@showResetForm')->name('password.reset');
+Route::get('password/reset/{token}','PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset','PasswordController@reset')->name('password.update');
