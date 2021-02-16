cd2318ec2f99e6d00eeb74d6b9b1b37f3ed64b47
vagrant <vagrant@homestead>
2021-02-16 12:02:50 +0000

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -93,13 +93,11 @@ class UsersController extends Controller
     {
         $view = 'email.confirm';
         $data = compact('user');
-        $from = 'zhoucg.com@gmail.com';
-        $name = 'zcg';
         $to = $user->email;
         $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
 
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
